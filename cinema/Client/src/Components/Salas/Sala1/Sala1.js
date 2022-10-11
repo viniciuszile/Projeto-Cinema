54cbab02c6dfba32ff3e9b5d1eb4e3957d874203
zile <89661612+viniciuszile@users.noreply.github.com>
2022-10-11 16:35:02 -0300

    Add files via upload

--- cinema/Client/src/Components/Salas/Sala1/Sala1.js
+++ cinema/Client/src/Components/Salas/Sala1/Sala1.js
@@ -1,34 +1,52 @@
 import "./Sala1.css"
 import React,{ useState,useEffect } from 'react';
-import Axios from "axios";
-
-
-
-
-
 
 export default function Sala1 (){
 
+
     const [ingressodisp, SetIngressodips] =  useState(100)
     const [ingressosvend, SetIngressosvend] = useState(0)
-    
+    const [faturamento, SetFaturamento] = useState(0)
+    const [programacao, SetProgramacao] = useState("Orfã : A origem")
+    const [duracao, SetDuracao] = useState("98 Minutos")
+    const [capacidade, SetCapacidade] = useState(100)
+
+
     const ingressos = () =>{
+
         SetIngressodips(ingressodisp - 1)
         SetIngressosvend(ingressosvend + 1)
+        SetFaturamento(ingressosvend * 10)
     }
+    
 
 
 
     return(
-        <div className="container-sala1">
-            <div className="infos">
+        
+        <div>
 
-                <h1 className="sala1-sala1">Sala 1 </h1>
-    
-                <p>Ingressos Disponiveis : {ingressodisp}</p>
-                <p>Ingressos vendidos : {ingressosvend}</p>
+            <div className="header-sala1">
+                    <h1>Cine-Zile</h1>
+                </div>
 
-                <button  className="botao-sala1" onClick={ingressos}>Comprar Ingresso</button>
+            <div className="container-sala1">
+
+                <div className="infos-sala1">
+
+                    <h1 className="texto_nome-sala1">Sala 1 </h1>
+
+                    
+                    <p className="texto_infos">Programação : {programacao}</p>
+                    <p className="texto_infos">Duração Do Filme : {duracao}</p>
+                    {/* <p>faturamento Sala : {faturamento}</p> */}
+                    <p className="texto_infos">Capacidade : {capacidade}</p>
+                    <p className="texto_infos">Ingressos Disponiveis : {ingressodisp}</p>
+                    <p className="texto_infos">Ingressos vendidos : {ingressosvend}</p>
+                    
+                    <button  className="botao-sala1" onClick={ingressos}>Comprar Ingresso</button>
+                    
+                </div>
             </div>
         </div>
     )
